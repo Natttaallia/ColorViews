1c590a4e2a9f2c2faf925f758ead3bc34f274389
Kulbaka Nataly <kulbacka.natalya@gmail.com>
2021-07-14 16:50:50 +0300

    initial commit

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: Array<TextView> =
+            arrayOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, red_button, green_button, yellow_button)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.design_default_color_error)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.purple_200)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.teal_700)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
